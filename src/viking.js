9fe93345f5528279b15ce7398f455b6edb33fb45
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-10-24 15:39:49 +0200

    Iteration 3 - Saxon and bonus part 1.

--- src/viking.js
+++ src/viking.js
@@ -36,13 +36,46 @@ class Viking extends Soldier {
         }
 
         return `${this.name} has died in act of combat`
+    }
 
+    battelCry() {
+        return 'Odin Owns You All!'
     }
 
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+
+        if(this.health > 0) {
+            return 'A Saxon has received ${damage} points of damage';
+        }
+
+        return 'A Saxon has died in combat';
+    }
+}
 
 // War
-class War {}
+class War {
+    vikingArmy
+    saxonArmy
+
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon(saxon) {
+        this.vikingArmy.push(saxon)
+    }
+}

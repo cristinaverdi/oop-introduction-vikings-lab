86668c74fb6e501c25805c75b5d6faf52a341545
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-10-24 10:48:39 +0200

    Iteration 2 - Viking receives damage and returns message

--- src/viking.js
+++ src/viking.js
@@ -22,6 +22,22 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
+    name
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage)
+
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        }
+
+        return `${this.name} has died in act of combat`
+
+    }
 
 }
 
